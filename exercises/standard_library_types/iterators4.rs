49966ecca3c55813f4bcd9899bf517c877469107
Micah Resnick <resnickmicah@gmail.com>
2020-12-05 22:42:17 -0500

    Finish arc, box, iterators

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,8 +1,8 @@
 // iterators4.rs
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
+    (1..num+1).fold(1, |acc, x| acc * x)
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
